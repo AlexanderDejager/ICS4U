ea19332b22e060ecdd4cbdf934ebb62270c6f363
AlexanderDejager <adejager@bayviewglen.ca>
2014-09-26 19:23:53 -0400

    change

--- QueensAssignment/src/com/bayviewglen/queens/Queens.java
+++ QueensAssignment/src/com/bayviewglen/queens/Queens.java
@@ -7,16 +7,24 @@ import java.util.Stack;
 public class Queens {
 
 	public static void main(String[] args) {
-
+		
+		boolean checked = false;
+		boolean nQueenPossible = false ;
 		int n = 0;
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("please enter a value for n: ");
 		n = input.nextInt();
+		
 
 		String Arr[][] = new String[n][n];
 		
+		int indexX=0;
+		int indexY=n-1;
+		
 		Stack<String> queenQue = new Stack<String>();
+		
+		
 
 		//this creates the blank chess board
 		for(int c = 0; c < n; c ++){
@@ -24,9 +32,37 @@ public class Queens {
 			for (int r = 0; r < n; r++){
 				Arr[c][r]="X";
 			}
+			
 		}
-		//code for placing a Q
-		//Arr[n-1][0] = "Q";
+		
+		
+	
+		while(checked == false){
+			
+			Arr[indexY][indexX] = "Q";
+			queenQue.push(Integer.toString(indexX));
+			queenQue.push(Integer.toString(indexY));
+			
+			if(Arr[indexY][indexX].equals("Q")){
+				System.out.println("yes");
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+		}
+		
+		
+		
+		
+			
+		
+		
 		
 		//this is responsible for printing the chess board
 		for(int c = 0; c < n; c ++){
