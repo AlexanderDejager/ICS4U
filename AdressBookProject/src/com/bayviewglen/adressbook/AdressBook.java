b254c14cd2b596f1c02e43db6afeef1d4353ef9d
AlexanderDejager <adejager@bayviewglen.ca>
2014-10-08 20:54:49 -0400

    hahah

--- AdressBookProject/src/com/bayviewglen/adressbook/AdressBook.java
+++ AdressBookProject/src/com/bayviewglen/adressbook/AdressBook.java
@@ -1,5 +1,10 @@
 package com.bayviewglen.adressbook;
 
+import java.util.HashMap;
+
 public class AdressBook {
+	
+	HashMap<String,Integer> contactRepository = new HashMap<String,Integer>();
+	
 
 }
--- AdressBookProject/src/com/bayviewglen/adressbook/AdressBookProgram.java
+++ AdressBookProject/src/com/bayviewglen/adressbook/AdressBookProgram.java
@@ -3,7 +3,25 @@ package com.bayviewglen.adressbook;
 public class AdressBookProgram {
 
 	public static void main(String[] args) {
-		System.out.println("fuck the world");
+		System.out.println("|---------------------------------------------------------------|");
+		System.out.println("|                Welcome To The Adress Book Menu                |");
+		System.out.println("|---------------------------------------------------------------|");
+		System.out.println("|Please Select a number                                         |");
+		System.out.println("|---------------------------------------------------------------|");
+		System.out.println("| 1.Add Contact");
+		System.out.println("| 2.");
+		System.out.println("| 3.");
+		System.out.println("| 4.");
+		System.out.println("| 5.");
+		System.out.println("| 6.");
+		System.out.println("| 7.");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		System.out.println("|");
+		
 
 	}
 
