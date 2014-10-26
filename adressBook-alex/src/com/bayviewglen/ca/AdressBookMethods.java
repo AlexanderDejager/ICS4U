c3b70468c3ab55e0a72b0380b39261daa7654718
AlexanderDejager <adejager@bayviewglen.ca>
2014-10-25 21:55:06 -0400

    delete contact method finished

--- adressBook-alex/src/com/bayviewglen/ca/AdressBookMethods.java
+++ adressBook-alex/src/com/bayviewglen/ca/AdressBookMethods.java
@@ -12,5 +12,25 @@ public class AdressBookMethods {
 	public void createContact(String first ,String last , String num){
 		adressBookStorage.put(num , new Contact(first , last, num));
 	}
+	
 
+
+	
+
+	
+	//method responsible for deleting the contact
+	public void deleteContact(Contact delete){
+		//this variable holds the key we want
+		String key = null;
+		//for each loop that finds the contact to delete , along with its key
+			for(String searchItem : adressBookStorage.keySet()){
+				if(adressBookStorage.containsKey(delete.getPhoneNumber())){
+					key = searchItem;
+				}
+				//removes the contact by using the key
+				adressBookStorage.remove(key);
+				System.out.println("Contact Was Deleted !");
+			}
+		
+	}
 }
