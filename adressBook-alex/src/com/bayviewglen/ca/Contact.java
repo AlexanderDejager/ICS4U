48afa0763510e46001d09fd8cfdc4b40ca166feb
AlexanderDejager <adejager@bayviewglen.ca>
2014-10-25 20:36:40 -0400

    created contact object

--- adressBook-alex/src/com/bayviewglen/ca/Contact.java
+++ adressBook-alex/src/com/bayviewglen/ca/Contact.java
@@ -4,7 +4,13 @@ public class Contact {
 	private String firstName;
 	private String LastName;
 	private String PhoneNumber;
-
+	
+	public Contact(){
+		firstName = null;
+	    LastName = null;
+	    PhoneNumber = null;
+		
+	}
 
 	
 	
