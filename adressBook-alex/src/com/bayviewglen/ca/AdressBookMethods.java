b38bdba1ee8ba777e133bb7814458db716ad383f
AlexanderDejager <adejager@bayviewglen.ca>
2014-10-25 21:28:25 -0400

    hash map created

--- adressBook-alex/src/com/bayviewglen/ca/AdressBookMethods.java
+++ adressBook-alex/src/com/bayviewglen/ca/AdressBookMethods.java
@@ -1,5 +1,12 @@
 package com.bayviewglen.ca;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class AdressBookMethods {
+	
+	Map<String,Contact> adressBookStorage = new HashMap<String,Contact>();
+	
+	//method that adds contacts
 
 }
