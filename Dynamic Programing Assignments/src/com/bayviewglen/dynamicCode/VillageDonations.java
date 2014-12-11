a59ca623deae7bde8957565193340ac180482599
AlexanderDejager <adejager@bayviewglen.ca>
2014-12-10 22:30:08 -0500

    created some storage stuff

--- Dynamic Programing Assignments/src/com/bayviewglen/dynamicCode/VillageDonations.java
+++ Dynamic Programing Assignments/src/com/bayviewglen/dynamicCode/VillageDonations.java
@@ -1,17 +1,20 @@
 package com.bayviewglen.dynamicCode;
 
-
 public class VillageDonations {
 
 	public static void main(String[] donated) {
 		// put whatever values you want into donations to test the program
 		int[] donations = { 1, 2, 3, 8, 7, 11 };
-
+		System.out.println(maxDonations(donations));
 	}
 
 
-
 	private static int maxDonations(int[] donated) {
+
+		// to check if i get the real value
+		int donationsStorage1[] = new int[donated.length];
+		// also just a second one to make double sure
+		int donationStorage2[] = new int[donated.length];
 
 		return 0;
 		}
