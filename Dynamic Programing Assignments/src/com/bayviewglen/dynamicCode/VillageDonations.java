d42ae17319594b7c5cb6b93e19daec1692e4e2cf
AlexanderDejager <adejager@bayviewglen.ca>
2014-12-10 22:28:43 -0500

    framework is done

--- Dynamic Programing Assignments/src/com/bayviewglen/dynamicCode/VillageDonations.java
+++ Dynamic Programing Assignments/src/com/bayviewglen/dynamicCode/VillageDonations.java
@@ -2,11 +2,17 @@ package com.bayviewglen.dynamicCode;
 
 
 public class VillageDonations {
-	
+
 	public static void main(String[] donated) {
-	
+		// put whatever values you want into donations to test the program
+		int[] donations = { 1, 2, 3, 8, 7, 11 };
+
 	}
 
-	
-	
-}
+
+
+	private static int maxDonations(int[] donated) {
+
+		return 0;
+		}
+	}
