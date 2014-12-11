4ed784a2f3cc27c640899c8761ee956a6f6ae9be
AlexanderDejager <adejager@bayviewglen.ca>
2014-12-11 00:55:41 -0500

    finished

--- Dynamic Programing Assignments/src/com/bayviewglen/dynamicCode/VillageDonations.java
+++ Dynamic Programing Assignments/src/com/bayviewglen/dynamicCode/VillageDonations.java
@@ -1,5 +1,11 @@
 package com.bayviewglen.dynamicCode;
 
+/**
+ * 
+ * @author aDejager
+ * @version 2 Gets the value of the village donation, using dynamic programming.
+ *          the Internet was god in this project.
+ */
 public class VillageDonations {
 
 	public static void main(String[] donated) {
@@ -8,6 +14,13 @@ public class VillageDonations {
 		System.out.println(maxDonations(donations));
 	}
 
+	/**
+	 * 
+	 * @param int[]
+	 * @return greatest donation you can receive from the town
+	 */
+	// the method that allows me to figure out the maximum donation i can
+	// receive
 	private static int maxDonations(int[] donated) {
 
 		// to check if i get the real value
@@ -17,7 +30,6 @@ public class VillageDonations {
 
 		// if no one is in the village you can not collect any donations
 		if (donated.length == 0) {
-
 			return 0;
 			// if there is only a single guy in the village his donation is the
 			// only one you can collect
@@ -26,32 +38,41 @@ public class VillageDonations {
 			// two guys ,we will choose the one willing to give more
 		} else if (donated.length == 2) {
 			return Math.max(donated[0], donated[1]);
+
 			// when there are three people , all of them are neighbors , takes
 			// the biggest of the three and returns it
 		} else if (donated.length == 3) {
 			return Math.max(Math.max(donated[0], donated[1]), donated[2]);
 			// dealing with all the other cases
 		} else {
-
+			// set both of the bellow to first value because the loop will start
+			// at 2
 			donationsStorage1[0] = donated[0];
 			donationsStorage1[1] = donated[0];
 
+			// iterates through the whole thing to find the biggest value
+			// between
+			// adding the current value of the house and the house two houses
+			// down
+			// or the current house
 			for (int i = 2; i < donated.length - 1; i++)
-
+				// will not remember the index,
+				// however stores the current biggest value
 				donationsStorage1[i] = Math.max(donationsStorage1[i - 2]
 						+ donated[i], donationsStorage1[i - 1]);
-
+			// Repeating this this time however we start with the second house
+			// rather than the first
 			donationStorage2[0] = donated[1];
 			donationStorage2[1] = donated[1];
 			donationStorage2[2] = Math.max(donated[1], donated[2]);
-
+			// again we iterate through for the value
 			for (int i = 2; i < donated.length; i++)
 				donationStorage2[i] = Math.max(donationStorage2[i - 2]
 						+ donated[i], donationStorage2[i - 1]);
-
+			// we are returning the greatest value between the two possible
+			// values
 			return Math.max(donationsStorage1[donationsStorage1.length - 2],
 					donationStorage2[donationStorage2.length - 1]);
 		}
 	}
-
 }
